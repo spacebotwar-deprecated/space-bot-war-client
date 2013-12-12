dc9a9149f6f26b85399653aa7e3a0c6034f7139d
Ian Docherty <windows@iandocherty.com>
2013-12-12 17:24:55 -0500

    Initial version (using a boilerplate framework)

---
+++ public/js/app/App.js
@@ -0,0 +1,58 @@
+define([    'jquery',   'backbone', 'marionette',   'underscore',   'handlebars', 'jquery.json'],
+function (   $,          Backbone,   Marionette,     _,              Handlebars) {
+    var App = new Backbone.Marionette.Application();
+
+    function isMobile() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        return ((/iPhone|iPod|iPad|Android|BlackBerry|Opera Mini|IEMobile/).test(userAgent));
+    }
+
+    //Organize Application into regions corresponding to DOM elements
+    //Regions can contain views, Layouts, or subregions nested as necessary
+    App.addRegions({
+        headerRegion:"header",
+        mainRegion:"#main"
+    });
+
+    App.addInitializer(function () {
+        Backbone.history.start();
+    });
+
+    App.mobile = isMobile();
+    var ws;
+
+    // Set up an event aggregator for the web socket
+    // 
+    var init_web_socket = function() {
+        var wsUrl = window.wsUrl || "ws://spacebotwar.com:5000/ws/game/lobby";
+        ws = new WebSocket(wsUrl);
+
+        ws.onerror = function(e) {
+            App.vent.trigger("ws:error", e);
+            console.log("ws:error",e);
+        };
+
+        ws.onopen = function() {
+            App.vent.trigger("ws:connected");
+            console.log("ws:connected");
+        };
+
+        ws.onmessage = function(e) {
+            var data    = $.evalJSON(e.data);
+            var type    = data.type;
+            var content = data.content;
+            App.vent.trigger("ws:"+type, content);
+            console.log("ws:"+type, content);
+        };
+            
+    }
+
+    App.addInitializer(init_web_socket);
+
+    App.vent.on("ws:connected", function(data) {
+        console.log("ws:connected - triggered");
+    });
+ 
+    return App;
+});
+
