7c135352fec36bf9cb8ecd9fa7fe6d61875e6f28
Ian Docherty <git@iandocherty.com>
2013-12-13 13:29:58 +0000

    Added a welcome page.

--- public/js/app/templates/helpers/yeller.js
+++ public/js/app/templates/helpers/yeller.js
@@ -1,7 +1,7 @@
 define(['handlebars'], function ( Handlebars ){
     function yeller ( context, options ) {
-        // Assume it's a string for simplicity.
-        return context + "!!!!!!!!";
+	// Assume it's a string for simplicity.
+	return context + "!!!!!!!!";
     }
 
     Handlebars.registerHelper( 'yeller', yeller );
