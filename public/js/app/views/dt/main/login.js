c363bf5ba5972adb1345a19b36b5e3c86eab1867
Ian Docherty <windows@iandocherty.com>
2013-12-14 16:46:00 -0500

    Finally! Got some sort of login from LoginRadius (see login.html)

--- public/js/app/views/dt/main/login.js
+++ public/js/app/views/dt/main/login.js
@@ -1,15 +1,15 @@
 define( [   'App',  'backbone', 'marionette',   'jquery',   'login-radius',             'models/Model', 'hbs!templates/all/main/login'],
-function(    App,    Backbone,   Marionette,     $,          LoginRadius_SocialLogin,    Model,          template) {
+function(    App,    Backbone,   Marionette,     $,          LoginRadius,    Model,          template) {
     //ItemView provides some default rendering logic
 
-    LoginRadius_SocialLogin.util.ready(function () {
-        $ui                         = LoginRadius_SocialLogin.lr_login_settings;
-        $ui.interfacesize           = "";
-        $ui.apikey                  = "78855680-671c-4c70-b194-2e392a0a7c81";
-        $ui.callback                = "";
-        $ui.lrinterfacecontainer    = "interfacecontainerdiv";
-        LoginRadius_SocialLogin.init( { login : true } );
-    });
+    LoginRadius.onlogin = SuccessFullyLogin;
+
+    function SuccessFullyLogin() {
+        LoginRadius.getUserprofile(function (data) {
+            alert(JSON.stringify(data));
+        });
+        return false;
+    };
 
     return Backbone.Marionette.ItemView.extend( {
         template    : template,
---
+++ public/js/app/views/dt/main/radius.js
@@ -0,0 +1,15 @@
+<script src="https://hub.loginradius.com/include/js/LoginRadius.js" ></script>
+<script type="text/javascript">      
+    var options={};      
+    options.login=true;       
+    LoginRadius_SocialLogin.util.ready(function () { 
+        $ui = LoginRadius_SocialLogin.lr_login_settings;
+        $ui.interfacesize = "";
+        $ui.apikey = "78855680-671c-4c70-b194-2e392a0a7c81";
+        $ui.callback=""; 
+        $ui.lrinterfacecontainer ="interfacecontainerdiv"; 
+        LoginRadius_SocialLogin.init(options);
+    }); 
+</script>
+
+https://hub.loginradius.com/cdn/include/js/LoginRadius.1.0.js
