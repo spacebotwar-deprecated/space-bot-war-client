35cd6495915d087889d4a5f5efa313ad6e95d388
Iain C Docherty <github@iain-c-docherty.com>
2014-01-02 17:07:21 +0000

    Finally got canvas image and size correct

--- public/js/app/views/dt/main/welcome.js
+++ public/js/app/views/dt/main/welcome.js
@@ -13,27 +13,23 @@ function(    App,    Backbone,   Marionette,     $,          Model,          tem
             var self = this;
             this.$el.empty().html(template);
            
-//            this.$( ".draggable" ).draggable({containment: "parent"});
+            this.$( ".draggable_canvas" ).draggable({containment: "parent"});
             var canvas = this.$("#draggable_canvas")[0];
             // TODO I have no idea why, but at this point canvas is 300x150
             // so I have to set it to this size. Note, default is 300x150
             // so perhaps that has something to do with it
-            canvas.height = 1024;
-            canvas.width = 1024;
-            this.context = canvas.getContext('2d');
-            this.starmap = new Image();
-            this.starmap.src="/img/starmap1.jpg";
-            this.starmap.onload = function() {
-                self.context.drawImage(self.starmap, 0, 0, 1024, 1024);
-            this.context.fillStyle = "rgb(150,29,28)";
-            this.context.beginPath();
-            this.context.arc(12,12,20,0,0,true);
-            this.context.closePath();
-
-            };
-
-//            this.context.drawImage(image_starmap, 0, 0, 200,200);
-
+            canvas.height = 3000;
+            canvas.width = 3000;
+            var context = this.context;
+            context = canvas.getContext('2d');
+            context.clearRect(0,0,3000,3000);
+//            context.drawImage(image_starmap, 0, 0, 1024, 1024);
+            context.beginPath();
+            context.moveTo(188,130);
+            context.bezierCurveTo(140,10,388,10,388,170);
+            context.lineWidth = 10;
+            context.strokeStyle = "red";
+            context.stroke();
         },
 
         // View Event Handlers
