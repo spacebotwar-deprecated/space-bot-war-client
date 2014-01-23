cab94300e3046ff8e3b48145aa4b6b8eb2202cc9
Vasari <nmccallum7@gmail.com>
2014-01-24 08:48:00 +1000

    Ported dev-server.js to coffee. More improvements to bin/sbw.pl

--- bin/sbw.pl
+++ bin/sbw.pl
@@ -13,23 +13,28 @@ my $task = $ARGV[0] // "";
 my $commands;
 
 given ($task) {
-    when (m/(build|otherTask)/i) {
+    when (/(build|otherGulpTask)/i) {
         $commands = qq{
             cd ..;
             gulp --require coffee-script $1;
         }; 
     }
-    when (m/test|runtests|t/i) {
+    when (/test|t/i) {
         $commands = qq{
             echo "Testng not implemented!";
         };
     }
-    when (m/runserver|server|dev-server/i) {
+    when (/server/i) {
+        my $port = $ARGV[1] // 8002;
         $commands = qq{
             echo "Warning: dev-server implementation still in progress!";
             cd ../dev-server
-            PORT=8003 nodemon dev-server.js
+            PORT=$port nodemon dev-server.coffee
         };
+    }
+    when (/help/i) {
+        display_help_message();
+        exit;
     }
     default {
         if ($_) {
@@ -44,9 +49,24 @@ given ($task) {
 
 system $commands;
 
+
+sub display_help_message {
+    my $message = qq{
+    Usage: ./sbw.pl <task> <that task's options>
+
+    Available tasks:
+        build      builds the js code, basically a wrapper around 'gulp build'
+        test       not implemented.
+        server     runs the development server, can pass in the port to run on
+        help       you're looking at it.
+
+    };
+    print $message;
+}
+
 END {
     # For fun. Who cares? .... *Right?*
-    say '';
+    say "\n";
     say 'Thanks for visiting. Do come again!';
-    say '';
+    say "\n";
 }
---
+++ dev-server/dev-server.coffee
@@ -0,0 +1,26 @@
+express   = require 'express'
+app       = express()
+http      = require 'http'
+path      = require 'path'
+port      = if process.env.PORT? then process.env.PORT else 8001
+servePath = path.join __dirname, '..', 'public'
+
+# Server Configuration
+app.configure () ->
+
+    # Tell express to serve files from /public.
+    app.use express.static servePath
+    
+    # Complain as loudly as possible when an error occurs.
+    app.use express.errorHandler
+        dumpExceptions:  true
+        showStack:       true
+    
+
+# Start Node.js Server
+server = http.createServer app
+server.listen port
+
+console.log 'Welcome to SpaceBotWar!'
+console.log "Serving files from #{servePath}."
+console.log "Hit http://localhost:#{port} to start debugging!!"
