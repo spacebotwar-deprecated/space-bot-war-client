f13061e55c416f781144f5fc7aec1f802c540b50
Vasari <nmccallum7@gmail.com>
2014-01-28 09:50:00 +1000

    Delete old js files.

--- src/js-app/collections/competitors.js
+++
@@ -1,8 +0,0 @@
-define([    "jquery",   "backbone", "models/competitor"],
-function(   $,          Backbone,   Competitor) {
-    var Competitors = Backbone.Collection.extend({
-        model: Competitor
-    });
-
-    return Competitors;
-});
--- src/js-app/views/dt/main/footer.js
+++
@@ -1,7 +0,0 @@
-define([    'jquery',   'hbs!templates/dt/main/footer', 'backbone', 'marionette'],
-function (   $,          template,                       Backbone) {
-    //ItemView provides some default rendering logic
-    return Backbone.Marionette.ItemView.extend({
-        template:template
-    });
-});
