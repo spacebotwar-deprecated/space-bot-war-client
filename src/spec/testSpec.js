41d5dfcca035a7b529c15bb58cfebf493d07964c
Vasari <nmccallum7@gmail.com>
2014-02-21 23:24:08 +1000

    Got Jasmine testing to work using a minimal example.

---
+++ src/spec/testSpec.js
@@ -0,0 +1,10 @@
+// Test for testing! Pro'lly not that amazing.
+//
+
+describe('a test to test that stuff works', function() {
+
+    it('should return true', function() {
+        expect(true).toBe(true);
+    });
+
+});
